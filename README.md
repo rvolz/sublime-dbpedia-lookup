2c0de269b4b53f07c19fd6847091ec311b8b212d
Rainer Volz <dev@textmulch.de>
2017-11-02 22:07:20 +0100

    Import

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # sublime-dbpedia-lookup
 A  Sublime Text 3 plugin for the DBpedia Lookup service
+
+This plugin uses the [DBpedia Lookup](https://github.com/dbpedia/lookup) service for keyword searches.   
+
+## Usage
+
+The plugin provides a command in the context menu:
+
+* right-click on a text selection for the context menu, then select _DBpedia Lookup_ to look up the selection
+* right-click on the cursor position for the context menu, then select _DBpedia Lookup_ to look up the surrounding word
+
+![Selecting for lookup](docs/ipsum1.png)
+
+After that ...
+
+* the plugin will search the DBpedia Lookup service for possible hits
+* if there were hits the results will be presented in a scratch window named _DBpedia Lookup_
+* all links in the result window can be right-clicked to open them in a browser, for further study
+
+![Result view](docs/ipsum2.png)
+
+## Server configuration
+
+By default the plugin will use the public endpoint for the DBpedia Lookup service at http://lookup.dbpedia.org. However, it is possible to also configure a _local_ endpoint. The DBpedia project provides a [Docker image](https://hub.docker.com/r/dbpedia/lookup/) that makes it possible to run the service on your own machine or elsewhere.
+
+To use the local endpoint configure the use preferences accordingly:
+
+* change the setting _default_server_ to _local_. The default is _remote_.
+* change the setting _dbpedia_lookup_server_local_ to the address/port of your machine. The default is _http://localhost:1111_, which is the address the Docker image uses.
+
+## Licence
+
+This plugin is freely available under the [MIT license](LICENSE).
+
+
---
+++ dbpedia_lookup.py
@@ -0,0 +1,88 @@
+import sublime
+import sublime_plugin
+from urllib.request import urlopen, Request, HTTPError, URLError
+import json
+import io
+import logging, pprint
+
+logging.basicConfig(level=logging.WARNING, format="[%(asctime)s - %(levelname)-8s - %(name)s] %(message)s")
+logger = logging.getLogger('DBpedia Lookup')
+
+class DbpediaLookupCommand(sublime_plugin.TextCommand):
+
+	VIEW_NAME = 'DBpedia Lookup'
+
+	def run(self, edit):
+		sels = self.view.sel()
+		if sels[0].empty(): # no selection, try to find the surronding word
+			lookup_region = self.view.word(sels[0])
+			lookup_item = self.view.substr(lookup_region)
+		else: # selection found
+			lookup_item = self.view.substr(sels[0])	
+		logger.info('DBpedia: looking up '+lookup_item)
+		results = self.lookup(lookup_item)
+		if results == None or not results['results']:
+			logger.warning('DBpedia: no results')
+			return
+		contents = self.output(results, lookup_item)
+		f = self.get_view()
+		f.set_read_only(False)
+		f.run_command('select_all')
+		f.run_command('right_delete')
+		f.insert(edit, 0, contents)
+		f.set_read_only(True)
+		f.run_command('fold_all')
+		sublime.active_window().focus_view(f)
+
+	def output(self, results, lookup_item):
+		output = io.StringIO()
+		output.write('Looked up: '+lookup_item+'\n\n')
+		for r in results['results']:
+			output.write(r['label']+': '+r['uri']+'\n')
+			output.write('  description: '+(r['description'] or '')+'\n')
+			output.write('  reference counter: '+str(r['refCount'])+'\n')
+			for a in ['categories', 'classes', 'templates', 'redirects']:
+				output.write('  '+a+':\n')
+				for b in r[a]:
+					output.write('    '+b['label']+': '+b['uri']+'\n')
+		contents = output.getvalue()
+		output.close()
+		return contents
+
+	def get_view(self, name = VIEW_NAME):
+		dlv = None
+		for w in sublime.windows():
+			for v in w.views():
+				if v.name() == name:
+					dlv = v
+					break
+		if dlv == None:
+			f = sublime.active_window().new_file()
+			f.set_encoding('utf-8')
+			f.set_name(name)
+			f.set_scratch(True)
+			f.set_syntax_file('Packages/YAML/YAML.tmLanguage')
+			return f
+		else:
+			return dlv
+
+	def lookup(self, name):
+		settings = sublime.load_settings('dbpedia-lookup.sublime-settings')
+		if settings.get('default_server', 'remote') == 'remote':
+			host = settings.get('dbpedia_lookup_server_remote', 'http://lookup.dbpedia.org')
+		else:
+			host = settings.get('dbpedia_lookup_server_local')
+		try:
+			headers = {'Accept': 'application/json'}	
+			req = Request(host+'/api/search/KeywordSearch?QueryClass=&MaxHits=5&QueryString='+name, None, headers)
+			resp = urlopen(req)
+			s = resp.read().decode('utf-8')
+			return json.loads(s)
+		except HTTPError as e:
+			err = '%s: HTTP error %s contacting API' % (__name__, str(e.code))
+			sublime.error_message(err)
+			return None
+		except URLError as e:
+			err = '%s: URL error %s contacting API' % (__name__, str(e.reason))
+			sublime.error_message(err)
+			return None
